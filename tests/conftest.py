7d46fc9a0c8468c88ebc93a2cc6a1b3f4710ff50
Gontzal Monasterio <go.monasterio@gmail.com>
2025-11-29 20:03:55 +0100

    chore: rename fixture and remove print

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,13 +7,12 @@ from syrupy.filters import props
 
 # Mock secrets.token_hex to return a consistent value for snapshot testing
 @pytest.fixture(autouse=True)
-def mock_secrets_token_hex(mocker: MockerFixture) -> None:
+def secrets_token_hex(mocker: MockerFixture) -> None:
     _ = mocker.patch("secrets.token_hex", return_value="MOCKED_TOKEN_HEX_VALUE")
 
 
 @pytest.fixture
 def snapshot_json(snapshot: SnapshotAssertion) -> SnapshotAssertion:
-    print(type(snapshot))
     # Exclude dynamic properties that change with every asset build or deployment
     # S3Key: Changes when Zip-based Lambda source code changes
     # ImageUri: Changes when Docker-based Lambda source code changes
